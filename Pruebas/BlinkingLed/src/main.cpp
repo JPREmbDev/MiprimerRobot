c50004fba9d52bc8924323eb6dec9999c350e7e0
JPREmbDev <121400656+JPREmbDev@users.noreply.github.com>
2024-08-20 23:13:37 +0200

    GPIO RESISTOR PULLDOWN/PULL UP

--- Pruebas/BlinkingLed/src/main.cpp
+++ Pruebas/BlinkingLed/src/main.cpp
@@ -4,7 +4,7 @@
 #include <driver/gpio.h>
 
 constexpr gpio_num_t LED_PIN = GPIO_NUM_2;
-//constexpr gpio_num_t INPUT_PIN = GPIO_NUM_32; // push button
+constexpr gpio_num_t INPUT_PIN = GPIO_NUM_32; // push button
 
 // Añadamimos el mangling 
 extern "C" void app_main();
@@ -12,7 +12,7 @@ extern "C" void app_main();
 void app_main() 
 {
     // Setup
-    // config ouput pin
+    // Config ouput pin LED
     gpio_config_t config_output;
     config_output.mode = GPIO_MODE_OUTPUT;
     config_output.pin_bit_mask = (1ULL << LED_PIN);
@@ -21,19 +21,38 @@ void app_main()
     config_output.pull_up_en = GPIO_PULLUP_DISABLE;
     gpio_config(&config_output);
 
+    // Config input pin PULSADOR
+    /*
+        Esta config funciona de tal manera que cuando pulsamos le llega un 0:
+            config_input.pull_down_en = GPIO_PULLDOWN_DISABLE;
+            config_input.pull_up_en = GPIO_PULLUP_ENABLE;
+        
+        Esta config funciona de tal manera que cuando pulsamos le llega un 1:
+            config_input.pull_down_en = GPIO_PULLDOWN_ENABLE;
+            config_input.pull_up_en = GPIO_PULLUP_DISABLE;
+    */
+    gpio_config_t config_input;
+    config_input.mode = GPIO_MODE_INPUT;
+    config_input.pin_bit_mask = (1ULL << INPUT_PIN);
+    config_input.intr_type = GPIO_INTR_DISABLE;
+    config_input.pull_down_en = GPIO_PULLDOWN_ENABLE;
+    config_input.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&config_input);
 
 
     // Loop
     while(1)
     {
+        int level = gpio_get_level(INPUT_PIN);
+        if(level){
+            printf("Encender Led!\n");
+            gpio_set_level(LED_PIN, level);
+        } else {
+            printf("Apagar Led!\n");
+            gpio_set_level(LED_PIN, level);
+        }
+        vTaskDelay(pdMS_TO_TICKS(200));
 
-        printf("Led encendido\n");
-        gpio_set_level(LED_PIN, 1);
-        vTaskDelay(pdMS_TO_TICKS(500));
-        printf("Led apagado\n");
-        gpio_set_level(LED_PIN, 0);
-
-        vTaskDelay(pdMS_TO_TICKS(500));
 
     }
 
